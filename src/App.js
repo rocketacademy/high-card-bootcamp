75033bbf3ed7cceb0b5e9cf43180c14ee95fb08c
neokaiyuan <kai@rocketacademy.co>
2022-05-03 17:44:31 +0800

    Add starter code for High Card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
+import { makeShuffledDeck } from "./utils.js";
 
 class App extends React.Component {
+  constructor(props) {
+    // Always call super with props in constructor to initialise parent class
+    super(props);
+    this.state = {
+      // Set default value of card deck to new shuffled deck
+      cardDeck: makeShuffledDeck(),
+      // currCards holds the cards from the current round
+      currCards: [],
+    };
+    // This binding is necessary to make `this` work in the callback
+    this.dealCards = this.dealCards.bind(this);
+  }
+
+  dealCards() {
+    // this.state.cardDeck.pop() modifies this.state.cardDeck array
+    const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()];
+    this.setState({
+      currCards: newCurrCards,
+    });
+  }
+
   render() {
+    const currCardElems = this.state.currCards.map(({ name, suit }) => (
+      // Give each list element a unique key
+      <div key={`${name}${suit}`}>
+        {name}
+        {suit}
+      </div>
+    ));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      <div>
+        <h3>Cards</h3>
+        {currCardElems}
+        <br />
+        <button onClick={this.dealCards}>Deal</button>
       </div>
     );
   }
