e273fda0a0a3d2f38ae27859d9b8004f3d5fe492
boxyboxy <eugenengboxiang@gmail.com>
2022-09-26 07:38:26 +0800

    completed refactoring of component with loading of images as min went through in class

--- src/PlayingCards.js
+++ src/PlayingCards.js
@@ -2,19 +2,26 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { RANK_MAP, IMAGE_MAP } from "./constants";
+
 export class PlayingCards extends React.Component {
   mappingPlayerCards() {
     const displayCards = this.props.currCards.map(
-      ({ name, suit, image }, index) => (
-        <Col key={`${name}${suit}`}>
-          <img
-            className="cardImage"
-            src={process.env.PUBLIC_URL + image}
-            alt={`${name}${suit}`}
-          />
-        </Col>
-      )
+      ({ name, suit, image, rank }, index) => {
+        const mappedSrcName = `${suit.toLowerCase()}${RANK_MAP[rank]}`;
+        return (
+          <Col key={`${name}${suit}`}>
+            <img
+              className="cardImage"
+              src={IMAGE_MAP[mappedSrcName]}
+              alt={`${name}${suit}`}
+            />
+          </Col>
+        );
+      }
     );
+
+    console.log(displayCards);
     return displayCards;
   }
 
