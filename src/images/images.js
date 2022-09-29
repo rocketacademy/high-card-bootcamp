81b4a72f4f3bf9de65170dce7ea64b3f00d0f1f2
0mgABear <yeoyi94@gmail.com>
2022-09-29 17:14:48 +0800

    first commit

---
+++ src/images/images.js
@@ -0,0 +1,107 @@
+import twoClubs from "./2_of_clubs.png";
+import threeClubs from "./3_of_clubs.png";
+import fourClubs from "./4_of_clubs.png";
+import fiveClubs from "./5_of_clubs.png";
+import sixClubs from "./6_of_clubs.png";
+import sevenClubs from "./7_of_clubs.png";
+import eightClubs from "./8_of_clubs.png";
+import nineClubs from "./9_of_clubs.png";
+import tenClubs from "./10_of_clubs.png";
+import jackClubs from "./jack_of_clubs.png";
+import queenClubs from "./queen_of_clubs.png";
+import kingClubs from "./king_of_clubs.png";
+import aceClubs from "./ace_of_clubs.png";
+import twoHearts from "./2_of_hearts.png";
+import threeHearts from "./3_of_hearts.png";
+import fourHearts from "./4_of_hearts.png";
+import fiveHearts from "./5_of_hearts.png";
+import sixHearts from "./6_of_hearts.png";
+import sevenHearts from "./7_of_hearts.png";
+import eightHearts from "./8_of_hearts.png";
+import nineHearts from "./9_of_hearts.png";
+import tenHearts from "./10_of_hearts.png";
+import jackHearts from "./jack_of_hearts.png";
+import queenHearts from "./queen_of_hearts.png";
+import kingHearts from "./king_of_hearts.png";
+import aceHearts from "./ace_of_hearts.png";
+import twoDiamonds from "./2_of_diamonds.png";
+import threeDiamonds from "./3_of_diamonds.png";
+import fourDiamonds from "./4_of_diamonds.png";
+import fiveDiamonds from "./5_of_diamonds.png";
+import sixDiamonds from "./6_of_diamonds.png";
+import sevenDiamonds from "./7_of_diamonds.png";
+import eightDiamonds from "./8_of_diamonds.png";
+import nineDiamonds from "./9_of_diamonds.png";
+import tenDiamonds from "./10_of_diamonds.png";
+import jackDiamonds from "./jack_of_diamonds.png";
+import queenDiamonds from "./queen_of_diamonds.png";
+import kingDiamonds from "./king_of_diamonds.png";
+import aceDiamonds from "./ace_of_diamonds.png";
+import twoSpades from "./2_of_spades.png";
+import threeSpades from "./3_of_spades.png";
+import fourSpades from "./4_of_spades.png";
+import fiveSpades from "./5_of_spades.png";
+import sixSpades from "./6_of_spades.png";
+import sevenSpades from "./7_of_spades.png";
+import eightSpades from "./8_of_spades.png";
+import nineSpades from "./9_of_spades.png";
+import tenSpades from "./10_of_spades.png";
+import jackSpades from "./jack_of_spades.png";
+import queenSpades from "./queen_of_spades.png";
+import kingSpades from "./king_of_spades.png";
+import aceSpades from "./ace_of_spades.png";
+
+export const imageMap = {
+  twoClubs,
+  threeClubs,
+  fourClubs,
+  fiveClubs,
+  sixClubs,
+  sevenClubs,
+  eightClubs,
+  nineClubs,
+  tenClubs,
+  jackClubs,
+  queenClubs,
+  kingClubs,
+  aceClubs,
+  twoHearts,
+  threeHearts,
+  fourHearts,
+  fiveHearts,
+  sixHearts,
+  sevenHearts,
+  eightHearts,
+  nineHearts,
+  tenHearts,
+  jackHearts,
+  queenHearts,
+  kingHearts,
+  aceHearts,
+  twoDiamonds,
+  threeDiamonds,
+  fourDiamonds,
+  fiveDiamonds,
+  sixDiamonds,
+  sevenDiamonds,
+  eightDiamonds,
+  nineDiamonds,
+  tenDiamonds,
+  jackDiamonds,
+  queenDiamonds,
+  kingDiamonds,
+  aceDiamonds,
+  twoSpades,
+  threeSpades,
+  fourSpades,
+  fiveSpades,
+  sixSpades,
+  sevenSpades,
+  eightSpades,
+  nineSpades,
+  tenSpades,
+  jackSpades,
+  queenSpades,
+  kingSpades,
+  aceSpades,
+};
--- src/utils.js
+++ src/utils.js
@@ -20,28 +20,19 @@ const shuffleCards = (cards) => {
 };
 
 const makeDeck = () => {
-  // Initialise an empty deck array
   const newDeck = [];
-  // Initialise an array of the 4 suits in our deck. We will loop over this array.
   const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
-  // Loop over the suits array
   for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
-    // Store the current suit in a variable
     const currentSuit = suits[suitIndex];
 
-    // Loop from 1 to 13 to create all cards for a given suit
-    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
-    // This is an example of a loop without an array.
     for (let rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
       // By default, card name and card rank are the same as rankCounter
       let cardName = `${rankCounter}`;
       let cardRank = rankCounter;
 
-      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
       if (cardName === "1") {
         cardName = "Ace";
-        // Ace has higher rank than all other cards
         cardRank = 14;
       } else if (cardName === "11") {
         cardName = "Jack";
@@ -51,21 +42,34 @@ const makeDeck = () => {
         cardName = "King";
       }
 
-      // Create a new card with the current name, suit, and rank
       const card = {
         name: cardName,
         suit: currentSuit,
         rank: cardRank,
       };
 
-      // Add the new card to the deck
       newDeck.push(card);
     }
   }
 
-  // Return the completed card deck
   return newDeck;
 };
 
 // Export functionality to create a shuffled 52-card deck
 export const makeShuffledDeck = () => shuffleCards(makeDeck());
+
+export const rank_map = {
+  2: "Two",
+  3: "Three",
+  4: "Four",
+  5: "Five",
+  6: "Six",
+  7: "Seven",
+  8: "Eight",
+  9: "Nine",
+  10: "Ten",
+  11: "Jack",
+  12: "Queen",
+  13: "King",
+  14: "Ace",
+};
