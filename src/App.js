5d94bdd31301232ce77d24b1434bfa9ecb524981
Huang Yuhao <yuhao.huang@ntu.edu.sg>
2023-04-11 09:18:12 -0700

    finalized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { makeShuffledDeck } from "./utils.js";
+import PlayingCard from "./playingcard";
 
 class App extends React.Component {
   constructor(props) {
@@ -23,97 +24,137 @@ class App extends React.Component {
   determineWinner = () => {
     const { currCards } = this.state;
     let s1 = this.detSuiteRank(currCards[0]);
-    let s2 =this.detSuiteRank(currCards[1]);
+    let s2 = this.detSuiteRank(currCards[1]);
     const currWinner =
       currCards[0].rank === currCards[1].rank
-        ? (s1>s2 ? "Player 1":"Player 2")
+        ? s1 > s2
+          ? "Player 1"
+          : "Player 2"
         : currCards[0].rank > currCards[1].rank
         ? "Player 1"
         : "Player 2";
 
     this.setState({ currWinner });
-    if (currWinner==="Player 1"){
+    if (currWinner === "Player 1") {
       this.updateScore(1);
-    }else if(currWinner==="Player 2"){
+    } else if (currWinner === "Player 2") {
       this.updateScore(2);
     }
-    
   };
-  
 
-  detSuiteRank =(card)=>{
-  
-    switch(card.suit){
-      case("Hearts"):
-        return 3
-      case("Spades"):
-        return 4
-      case("Clubs"):
-        return 2
-      case("Diamonds"):
-        return 1
+  detSuiteRank = (card) => {
+    switch (card.suit) {
+      case "Hearts":
+        return 3;
+      case "Spades":
+        return 4;
+      case "Clubs":
+        return 2;
+      case "Diamonds":
+        return 1;
       default:
-        return null
+        return null;
     }
-  }
-  updateScore =(n)=>{
-    const {scoreboard} = this.state
-    if (n ===1){
-      scoreboard[0]++
-    }else if (n===2){
-      scoreboard[1]++
+  };
+  updateScore = (n) => {
+    const { scoreboard } = this.state;
+    if (n === 1) {
+      scoreboard[0]++;
+    } else if (n === 2) {
+      scoreboard[1]++;
     }
-    this.setState({scoreboard})
-  }
+    this.setState({ scoreboard });
+  };
 
   dealCards = () => {
     // this.state.cardDeck.pop() modifies this.state.cardDeck array
-    const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()];
-    this.setState({
-      currCards: newCurrCards,
-    },
-    ()=>{
-      // Call determineWinner after the state has been updated
-      this.determineWinner();
-
+    if (this.state.cardDeck !== []) {
+      const newCurrCards = [
+        this.state.cardDeck.pop(),
+        this.state.cardDeck.pop(),
+      ];
+      // console.log(this.state.cardDeck);
+      this.setState(
+        {
+          currCards: newCurrCards,
+        },
+        () => {
+          // Call determineWinner after the state has been updated
+          this.determineWinner();
+        }
+      );
+    } else {
+      return (
+        <p>
+          The deck is out of cards. Please click 'restart to restart the game.
+        </p>
+      );
     }
-    );
+  };
+  restartGame = () => {
+    this.setState({
+      cardDeck: makeShuffledDeck(),
+      currCards: [],
+      currWinner: null,
+      scoreboard: [0, 0],
+    });
   };
 
   render() {
     //Code to check the winner: if cardRank not same, return higher cardRank, if same, check for suite and return higher suite
-    
-
-    const currCardElems = this.state.currCards.map(({ name, suit }) => (
-      // Give each list element a unique key
+    const { cardDeck, currCards, currWinner, scoreboard } = this.state;
+    const currCardElems = currCards.map(({ name, suit }, index) => (
+      // Give each list element a unique key //{name} of {suit}
       <div key={`${name}${suit}`}>
-        {name} of {suit}
+        Player {index + 1} have: {PlayingCard(currCards[index])}
       </div>
     ));
 
-    const hideOutput =()=>{
-      if(this.state.currWinner){
-        return(
-        <div>
-          <h3>{this.state.currWinner} has won</h3>
-          <p>Player 1 has {this.state.scoreboard[0]} points.</p>
-          <p>Player 2 has {this.state.scoreboard[1]} points.</p>
-        </div>)
-      }else{
-        return (<div><p>Press 'Deal' to start the game.</p></div>)
+    const hideOutput = () => {
+      if (currWinner) {
+        return (
+          <div>
+            <h5>{currWinner} has won this round</h5>
+            <p>Player 1 has {scoreboard[0]} points.</p>
+            <p>Player 2 has {scoreboard[1]} points.</p>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <p>Press 'Deal' to start the game.</p>
+          </div>
+        );
       }
-    }
-         
+    };
+    const gameWinner =
+      scoreboard[0] > scoreboard[1] ? (
+        <h3>Player 1 has won this game</h3>
+      ) : scoreboard[0] < scoreboard[1] ? (
+        <h3>Player 2 has won this game</h3>
+      ) : (
+        <h3>Both players are tied</h3>
+      );
     return (
       <div className="App">
         <header className="App-header">
           <h3>High Card 🚀</h3>
           {currCardElems}
-          
-          <br />
-          <button onClick={this.dealCards}>Deal</button>
           {hideOutput()}
-          
+          <br />
+          {cardDeck.length === 0 ? (
+            <div>
+              {gameWinner}
+              <p>Press Restart to restart the game</p>
+            </div>
+          ) : (
+            <button onClick={this.dealCards}>Deal</button>
+          )}
+          <br />
+          <br />
+          {cardDeck.length === 0 ? <br /> : <p>Reset the game:</p>}
+
+          <button onClick={this.restartGame}>Restart</button>
         </header>
       </div>
     );
