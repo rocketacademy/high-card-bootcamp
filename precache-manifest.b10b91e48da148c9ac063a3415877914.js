72536743a3412850122a91a4daac4893895424f2
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-07-23 22:09:06 +0800

    Updates

---
+++ precache-manifest.b10b91e48da148c9ac063a3415877914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e52d6651f840a915ca77",
+    "url": "/high-card-bootcamp/static/css/main.51806373.chunk.css"
+  },
+  {
+    "revision": "e52d6651f840a915ca77",
+    "url": "/high-card-bootcamp/static/js/main.e52d6651.chunk.js"
+  },
+  {
+    "revision": "889dfe253636d8e7d48c",
+    "url": "/high-card-bootcamp/static/js/1.889dfe25.chunk.js"
+  },
+  {
+    "revision": "4d33eb52879270c2e2e8",
+    "url": "/high-card-bootcamp/static/js/runtime~main.4d33eb52.js"
+  },
+  {
+    "revision": "7f8e147371159ed45bb98f3d36e05726",
+    "url": "/high-card-bootcamp/index.html"
+  }
+];
