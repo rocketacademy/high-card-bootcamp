f5cb6cc73c7863e40c63327ba6d5a70778bc85b5
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-07-23 21:51:01 +0800

    feat: added game card images, and games tracker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { makeShuffledDeck, shuffleCards } from './utils.js';
 import Button from '@mui/material/Button';
@@ -10,7 +10,6 @@ const App = () => {
 	const [scores, setPlayerScores] = useState({ player1: 0, player2: 0 });
 	const [rounds, setRounds] = useState(0);
 	const [winner, setWinner] = useState('');
-
 	const [game, setGame] = useState({ player1: 0, player2: 0 });
 
 	const dealCards = () => {
@@ -22,6 +21,7 @@ const App = () => {
 		setTimeout(() => {
 			const player1Elem = document.getElementById('player1');
 			const player2Elem = document.getElementById('player2');
+
 			if (player1Elem && player2Elem) {
 				const player1Content = player1Elem.textContent;
 				const player2Content = player2Elem.textContent;
@@ -34,6 +34,7 @@ const App = () => {
 
 	const handleStrings = (card1, card2) => {
 		const cardValues = { A: 14, J: 11, Q: 12, K: 13 };
+		console.log(card1, card2);
 
 		if (typeof card1 === 'number' && typeof card2 === 'number') {
 			return [card1, card2];
@@ -46,6 +47,28 @@ const App = () => {
 			}
 			return [card1, card2];
 		}
+	};
+
+	const manageGameScore = () => {
+		const player1FinalScore = document
+			.querySelector('.scores :first-child')
+			.textContent.slice(-2);
+		const player2FinalScore = document
+			.querySelector('.scores :last-child')
+			.textContent.slice(-2);
+		if (Number(player1FinalScore) > Number(player2FinalScore)) {
+			setGame((game) => ({
+				...game,
+				player1: game.player1 + 1,
+			}));
+		} else {
+			setGame((game) => ({
+				...game,
+				player2: game.player2 + 1,
+			}));
+		}
+
+		setPlayerScores({ player1: 0, player2: 0 });
 	};
 
 	const awardScores = (score1, score2) => {
@@ -73,6 +96,7 @@ const App = () => {
 			setCardDeck([...discardPile]);
 			setDiscardPile([]);
 			shuffleCards(cardDeck);
+			manageGameScore();
 			setRounds(0);
 		}
 	};
@@ -81,9 +105,11 @@ const App = () => {
 		<div className='cardContainer' key={`${name}${suit}`}>
 			<img
 				className='card'
-				src={`/img/${name}${suit}.svg`}
+				src={`./img/${name}${suit}.svg`}
 				alt={`${name} of ${suit}`}
 			/>
+
+			<span id={`player${index + 1}`}>{name}</span>
 		</div>
 	));
 
@@ -98,7 +124,7 @@ const App = () => {
 						</span>
 					</h3>
 
-					<h4> round {rounds}</h4>
+					<h4> Round {rounds}</h4>
 					<div className='playingCards'>{currCardElems}</div>
 
 					<br />
@@ -120,6 +146,11 @@ const App = () => {
 					<div className='scores'>
 						<p> Player 1 score: {scores.player1}</p>
 						<p>Player 2 score: {scores.player2}</p>
+					</div>
+
+					<div>
+						<p>Player 1 games won: {game.player1}</p>
+						<p>Player 2 games won: {game.player2}</p>
 					</div>
 				</header>
 			</div>
