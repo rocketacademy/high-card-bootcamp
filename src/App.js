f379c191f958ef241fa4c5fc7cc5a1c60f78c536
jewc <jamescew@gmail.com>
2023-07-23 19:25:17 +0800

    High card game commit

--- src/App.js
+++ src/App.js
@@ -17,10 +17,34 @@ class App extends React.Component {
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
       currCards: [],
+      // Increment score for Player A, Player B, tie
+      scoreState: [0, 0, 0],
+      roundWinner: null, // winner of current round
+      pastWins: [0, 0],
     };
-    this.scoreState = [0, 0];
-    this.rounds = this.cardDeck.size();
   }
+
+  // restart Game
+  restartGame = () => {
+    const { scoreState, pastWins } = this.state;
+    if (scoreState[0] > scoreState[1]) {
+      pastWins[0] = pastWins[0] + 1;
+    } else {
+      pastWins[1] = pastWins[1] + 1;
+    }
+
+    this.setState({
+      // if player 1 wins, then increment pastWins[0] by 1, then increment pastWins[0] by 1
+      // {(scoreState[0] > scoreState[1])
+      //                     ? pastWins[0] = pastWins[0] + 1,
+      //                     : pastWins[1] = pastWins[1] + 1,
+
+      cardDeck: makeShuffledDeck(),
+      currCards: [],
+      scoreState: [0, 0, 0],
+      roundWinner: null,
+    });
+  };
 
   //
   dealCards = () => {
@@ -32,15 +56,21 @@ class App extends React.Component {
     this.setState({
       currCards: newCurrCards,
     });
+
+    // Update state somewhere else
     // determine which player has won the game
     if (this.state.currCards[0].rank > this.state.currCards[1].rank) {
-      // Player A wins, update the score
-      this.scoreState[0] = this.scoreState[0] + 1;
+      // Player A wins, update the score for player A
+      this.state.scoreState[0] = this.state.scoreState[0] + 1;
+      console.log("Player A wins");
     } else if (this.state.currCards[0].rank < this.state.currCards[1].rank) {
-      // Player B wins, update the score
-      this.scoreState[1] = this.scoreState[1] + 1;
+      // Player B wins, update the score for player B
+      this.state.scoreState[1] = this.state.scoreState[1] + 1;
+      console.log("Player B wins");
     } else {
       // Tie, no update to the score
+      this.state.scoreState[2] = this.state.scoreState[2] + 1;
+      console.log("Tie");
     }
   };
 
@@ -56,17 +86,51 @@ class App extends React.Component {
       </div>
     ));
 
+    const numRoundsLeft = this.state.cardDeck.length / 2;
+    const numRoundsLeftMsg = `There are ${numRoundsLeft} rounds left in the game!`;
+
     return (
       <div className="App">
         <header className="App-header">
           <h3>High Card 🚀</h3>
           {currCardElems}
-          <div>Remaining cards: </div>
           <div>
-            Player 1:{this.scoreState[0]} vs. Player 2:{this.scoreState[1]}
+            Round Number:{" "}
+            {this.state.scoreState[0] +
+              this.state.scoreState[1] +
+              this.state.scoreState[2] +
+              1}
+          </div>
+          <div>{numRoundsLeftMsg}</div>
+
+          <div>
+            Player A has won {this.state.scoreState[0]} rounds this game
+          </div>
+          <div>
+            Player B has won {this.state.scoreState[1]} rounds this game{" "}
           </div>
           <br />
-          <button onClick={this.dealCards}>Deal</button>
+          <div>
+            {
+              // Check if game over
+              numRoundsLeft === 0 ? (
+                <div>
+                  {" "}
+                  <div>Game Over</div>
+                  <div>
+                    {this.state.scoreState[0] > this.state.scoreState[1]
+                      ? "Player A wins"
+                      : "Player B wins"}
+                  </div>
+                  <button onClick={this.restartGame}>Restart</button>
+                </div>
+              ) : (
+                <button onClick={this.dealCards}>Deal</button>
+              )
+            }
+          </div>
+          <div>Player A has won {this.state.pastWins[0]} games in past</div>
+          <div>Player B has won {this.state.pastWins[1]} games in past </div>
         </header>
       </div>
     );
