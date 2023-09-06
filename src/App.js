1d16e04f6c1ded69640c96071e985a5ebf06a8cf
shanquanSQ <thiashanquan93@gmail.com>
2023-09-06 20:46:55 +0800

    before i mess up the file

--- src/App.js
+++ src/App.js
@@ -22,8 +22,42 @@ class App extends React.Component {
     });
   };
 
+  // const function checkScore(){
+
+  // }
+  // Returns Boolean based on drawn cards. true if Player wins, false if Computer wins.
+  // checkScore = () => {
+  //   console.log("started");
+  //   console.log(this.state.currCards[0].rank);
+  //   if (this.state.currCards[0].rank > this.state.currCards[1].rank) {
+  //     return <h1>you won!</h1>;
+  //   } else {
+  //     return <h1>you lost!</h1>;
+  //   }
+  // };
+  checkScore = () => {
+    console.log("started");
+    if (this.state.currCards[0].rank > this.state.currCards[1].rank) {
+      return <h1>hello</h1>;
+    } else {
+      return <h1>noo</h1>;
+    }
+  };
+
+  // OLD FUNCTION
+  // checkScore() {
+  //   console.log("started");
+  //   if (this.state.currCards[0].rank > this.state.currCards[1].rank) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
   render() {
     // You can write JavaScript here, just don't try and set your state!
+
+    ///// Not sure what kind of functions I can write here
 
     // You can access your current components state here, as indicated below
     const currCardElems = this.state.currCards.map(({ name, suit }) => (
@@ -39,7 +73,16 @@ class App extends React.Component {
           <h3>High Card 🚀</h3>
           {currCardElems}
           <br />
-          <button onClick={this.dealCards}>Deal</button>
+          <button
+            onClick={() => {
+              this.dealCards();
+            }}
+          >
+            Deal
+          </button>{" "}
+          {/* <button onClick={this.dealCards}>Deal</button>{" "} */}
+          <h2>{this.checkScore}</h2>
+          {/* <h2>{this.checkScore ? "you won" : "you lost"}</h2> */}
         </header>
       </div>
     );
