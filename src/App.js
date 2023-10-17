acbdb8a74d508fd700b2cec4ab8cb2dac1655097
ianlauyin <ianlauyin@gmail.com>
2023-10-17 20:48:53 +0800

    Added Table for player and cards

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import React from "react";
 import "./App.css";
 import { makeShuffledDeck } from "./utils.js";
@@ -17,20 +20,28 @@ class App extends React.Component {
   dealCards = () => {
     // this.state.cardDeck.pop() modifies this.state.cardDeck array
     const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()];
+
     this.setState({
       currCards: newCurrCards,
     });
   };
 
+  // compareCards = () =>{
+  //   if(this.state.currCards[0].rank>this.state.currCards[1].rank)
+
+  // }
+
   render() {
     // You can write JavaScript here, just don't try and set your state!
 
     // You can access your current components state here, as indicated below
-    const currCardElems = this.state.currCards.map(({ name, suit }) => (
-      // Give each list element a unique key
-      <div key={`${name}${suit}`}>
-        {name} of {suit}
-      </div>
+    const currCardElems = this.state.currCards.map(({ name, suit }, i) => (
+      <Row key={"Player" + (i + 1)} fluid>
+        <Col>Player {i + 1}</Col>
+        <Col>
+          {name} of {suit}
+        </Col>
+      </Row>
     ));
 
     return (
