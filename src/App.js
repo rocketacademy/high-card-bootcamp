9a688870ac29f94958edf2df49401f6f24189426
ianlauyin <ianlauyin@gmail.com>
2023-10-17 21:25:21 +0800

    Card change to base on player's object key

--- src/App.js
+++ src/App.js
@@ -13,42 +13,49 @@ class App extends React.Component {
       // Set default value of card deck to new shuffled deck
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
-      currCards: [],
+      player1: { score: 0, result: "", currCard: {} },
+      player2: { score: 0, result: "", currCard: {} },
     };
   }
 
   dealCards = () => {
     // this.state.cardDeck.pop() modifies this.state.cardDeck array
-    const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()];
-
+    const newCurrCard1 = this.state.cardDeck.pop();
+    const newCurrCard2 = this.state.cardDeck.pop();
     this.setState({
-      currCards: newCurrCards,
+      player1: { currCard: newCurrCard1 },
+      player2: { currCard: newCurrCard2 },
     });
   };
-
-  // compareCards = () =>{
-  //   if(this.state.currCards[0].rank>this.state.currCards[1].rank)
-
-  // }
 
   render() {
     // You can write JavaScript here, just don't try and set your state!
 
     // You can access your current components state here, as indicated below
-    const currCardElems = this.state.currCards.map(({ name, suit }, i) => (
-      <Row key={"Player" + (i + 1)} fluid>
-        <Col>Player {i + 1}</Col>
-        <Col>
-          {name} of {suit}
-        </Col>
-      </Row>
-    ));
+    const currCardElems = (
+      <div>
+        <Row fluid>
+          <Col>Player 1</Col>
+          <Col>
+            {this.state.player1.currCard.name} of{" "}
+            {this.state.player1.currCard.suit}
+          </Col>
+        </Row>
+        <Row fluid>
+          <Col>Player 2</Col>
+          <Col>
+            {this.state.player2.currCard.name} of{" "}
+            {this.state.player2.currCard.suit}
+          </Col>
+        </Row>
+      </div>
+    );
 
     return (
       <div className="App">
         <header className="App-header">
           <h3>High Card 🚀</h3>
-          {currCardElems}
+          {"name" in this.state.player1.currCard ? currCardElems : ""}
           <br />
           <button onClick={this.dealCards}>Deal</button>
         </header>
