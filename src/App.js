d0bb325a36a13f17a480c14d94b469597c69f5a6
Mannushka <zaimar1910@gmail.com>
2023-10-21 11:19:08 +0800

    step 1: declaring and dislpaying the winner of the round done

--- src/App.js
+++ src/App.js
@@ -11,25 +11,41 @@ class App extends React.Component {
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
       currCards: [],
+      hasGameStarted: false,
+      currRoundWinner: null,
+      playerOneNumOfWins: 0,
+      playerTwoNumOfWins: 0,
+      roundNumber: 0,
     };
   }
 
   dealCards = () => {
     // this.state.cardDeck.pop() modifies this.state.cardDeck array
-    const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()];
+    const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()]; // each player gets one card
     this.setState({
       currCards: newCurrCards,
     });
+    this.determineThisRoundWinner(newCurrCards);
+  };
+  //function to determine the winner of the round
+  determineThisRoundWinner = (currCards) => {
+    const newCurrRoundWinner =
+      currCards[0].rank === currCards[1].rank
+        ? "It's a tie!"
+        : currCards[0].rank > currCards[1].rank
+        ? "Player 1 wins!"
+        : "Player 2 wins!";
+    this.setState({ currRoundWinner: newCurrRoundWinner });
   };
 
   render() {
     // You can write JavaScript here, just don't try and set your state!
 
     // You can access your current components state here, as indicated below
-    const currCardElems = this.state.currCards.map(({ name, suit }) => (
+    const currCardElems = this.state.currCards.map(({ name, suit }, i) => (
       // Give each list element a unique key
       <div key={`${name}${suit}`}>
-        {name} of {suit}
+        Player {i + 1} got {name} of {suit}
       </div>
     ));
 
@@ -38,6 +54,8 @@ class App extends React.Component {
         <header className="App-header">
           <h3>High Card 🚀</h3>
           {currCardElems}
+
+          {this.state.currRoundWinner}
           <br />
           <button onClick={this.dealCards}>Deal</button>
         </header>
