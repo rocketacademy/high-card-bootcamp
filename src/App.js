e5a08feb9ad3de7bdc81dcfc690ac918f8ced599
Mannushka <zaimar1910@gmail.com>
2023-10-21 22:26:43 +0700

    number of rounds left and current round added

--- src/App.js
+++ src/App.js
@@ -35,15 +35,16 @@ class App extends React.Component {
     this.setState({
       currRoundWinner: newCurrRoundWinner,
       hasGameStarted: true,
+      roundNumber: this.state.roundNumber + 1,
     });
     this.keepScore(newCurrRoundWinner);
   };
   //function to keep track of the score
   keepScore = (currRoundWinner) => {
-    if (currRoundWinner == 1)
+    if (currRoundWinner === 1)
       this.setState({ playerOneNumOfWins: this.state.playerOneNumOfWins + 1 });
 
-    if (currRoundWinner == 2)
+    if (currRoundWinner === 2)
       this.setState({ playerTwoNumOfWins: this.state.playerTwoNumOfWins + 1 });
   };
 
@@ -59,20 +60,29 @@ class App extends React.Component {
         </p>
       </div>
     ));
+    const gameDirections = this.state.hasGameStarted
+      ? `Click "deal" to continue the game.`
+      : `Click "deal to start the game.`;
     const currRoundWinnerOutput = this.state.currRoundWinner
       ? `Player ${this.state.currRoundWinner} won this round.`
       : `It's a tie!`;
     const playerOneNumOfWinsOutput = `Player 1 has ${this.state.playerOneNumOfWins} wins.`;
     const playerTwoNumOfWinsOutput = `Player 2 has ${this.state.playerTwoNumOfWins} wins.`;
+    const numOfRoundsLeft = `Number of rounds left: ${
+      this.state.cardDeck.length / 2
+    }`;
 
     return (
       <div className="App">
         <header className="App-header">
-          <h3>High Card 🚀</h3>
+          <h3>Welcome to High Card game!🚀</h3>
+          <h5>{gameDirections}</h5>
+          {/* <p>{this.state.roundNumber}</p> */}
           {currCardElems}
           <p>{this.state.hasGameStarted && currRoundWinnerOutput}</p>
           <p>{this.state.hasGameStarted && playerOneNumOfWinsOutput}</p>
           <p>{this.state.hasGameStarted && playerTwoNumOfWinsOutput}</p>
+          <p>{numOfRoundsLeft}</p>
 
           <br />
           <button onClick={this.dealCards}>Deal</button>
