de2197eafd2852e6d67908922089ddeb19c67759
Lili00ani <saputri.liliani@gmail.com>
2023-10-21 11:29:40 +0800

    added reset and alert bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+// import Alert from "react-bootstrap/Alert";
+import { Alert } from "react-bootstrap";
 import "./App.css";
 import { makeShuffledDeck } from "./utils.js";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -33,22 +37,17 @@ class App extends React.Component {
     );
   };
 
-  countScore = () => {
-    const tempCounter = [0, 0];
-    //if indexWinner is 0, add +1 to count index 0, vice versa
-    if (this.state.indexWinner === 0) {
-      tempCounter[0]++;
-    } else if (this.state.indexWinner === 1) {
-      tempCounter[1]++;
-    }
-    console.log(this.state.counter);
-
-    this.setState((prevState) => ({
-      player1Score: prevState.player1Score + tempCounter[0],
-      player2Score: prevState.player2Score + tempCounter[1],
-    }));
-
-    console.log(tempCounter);
+  reset = () => {
+    this.setState({
+      cardDeck: makeShuffledDeck(),
+      // currCards holds the cards from the current round
+      currCards: [],
+      indexWinner: -1,
+      player1Score: 0,
+      player2Score: 0,
+      restart: false,
+      roundWinner: null,
+    });
   };
 
   determineWinner = () => {
@@ -76,11 +75,33 @@ class App extends React.Component {
     );
   };
 
-  checkRestart() {
-    if (this.state.cardDeck.length === 0) {
-      this.setState({ restart: true });
+  countScore = () => {
+    const tempCounter = [0, 0];
+    //if indexWinner is 0, add +1 to count index 0, vice versa
+    if (this.state.indexWinner === 0) {
+      tempCounter[0]++;
+    } else if (this.state.indexWinner === 1) {
+      tempCounter[1]++;
     }
-  }
+
+    this.setState((prevState) => ({
+      player1Score: prevState.player1Score + tempCounter[0],
+      player2Score: prevState.player2Score + tempCounter[1],
+    }));
+  };
+
+  checkWinner = () => {
+    let tempWinner = -1;
+    if (this.state.player1Score > this.state.player2Score) {
+      tempWinner = 0;
+    } else if (this.state.player2Score > this.state.player1Score) {
+      tempWinner = 1;
+    }
+
+    this.setState({
+      roundWinner: tempWinner,
+    });
+  };
 
   render() {
     // You can write JavaScript here, just don't try and set your state!
@@ -92,23 +113,32 @@ class App extends React.Component {
       </div>
     ));
 
+    const numRoundsLeft = this.state.cardDeck.length / 2;
+    const gameWinnerMessage = `🏆Player ${this.state.roundWinner + 1} WON!🏆`;
+
     return (
       <div className="App">
         <header className="App-header">
           <h3>High Card 🚀</h3>
-          {currCardElems}
+          <Alert variant="success">Player 1{currCardElems[0]}</Alert>
+          <Alert variant="success">Player 2{currCardElems[1]}</Alert>
+          {/* {currCardElems} */}
           <br />
           <button onClick={this.dealCards}>Deal</button>
           <h3>
-            {this.state.indexWinner == -1
+            {this.state.indexWinner === -1
               ? ""
-              : this.state.indexWinner == 99
+              : this.state.indexWinner === 99
               ? `Tie`
               : `Player ${this.state.indexWinner + 1} won!`}
           </h3>
-          <h3>
-            {this.state.player1Score}--{this.state.player2Score}
-          </h3>
+          <p>
+            Player 1 🏆{this.state.player1Score} -- Player 2 🏆
+            {this.state.player2Score}
+          </p>
+          <p>There are {numRoundsLeft} rounds left</p>
+          <h1>{numRoundsLeft === 0 && gameWinnerMessage}</h1>
+          {numRoundsLeft === 0 && <button onClick={this.reset}>Reset</button>}
         </header>
       </div>
     );
