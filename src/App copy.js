6c4c2df8e3f334e2732e3cf1346dcfa51799533d
Lili00ani <saputri.liliani@gmail.com>
2023-10-25 22:57:53 +0800

    updated with bootstrap layout

--- src/App copy.js
+++ src/App copy.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./App.css";
 import { makeShuffledDeck } from "./utils.js";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,21 +13,19 @@ class App extends React.Component {
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
       currCards: [],
-      //index of the winner per each round
       indexWinner: -1,
       player1Score: 0,
       player2Score: 0,
-      //the winner of the game in the end of 26 rounds
       roundWinner: null,
       gameStart: true,
-      //overall player score across multiple game sessions
       overallPlayer1Score: 0,
       overallPlayer2Score: 0,
+      overallWinner: null,
     };
   }
 
   dealCards = () => {
-    // dealing cards
+    // this.state.cardDeck.pop() modifies this.state.cardDeck array
     const newCurrCards = [this.state.cardDeck.pop(), this.state.cardDeck.pop()];
 
     this.setState(
@@ -40,9 +39,9 @@ class App extends React.Component {
   };
 
   reset = () => {
-    //reset every state except overall player score across multiple game sessions
     this.setState({
       cardDeck: makeShuffledDeck(),
+      // currCards holds the cards from the current round
       currCards: [],
       indexWinner: -1,
       player1Score: 0,
@@ -53,8 +52,8 @@ class App extends React.Component {
   };
 
   determineWinner = () => {
-    //compare rank of player 1 and player 2 card
-    //return winner, either index 0 (player 1) or 1 (player 2) or 99 (tie).
+    //compare rank
+    //return winner, either index 0 or 1.
     let temp = -1;
 
     if (this.state.currCards === null) {
@@ -67,7 +66,6 @@ class App extends React.Component {
       temp = 99;
     }
 
-    //trigger function to count the total score
     this.setState(
       {
         indexWinner: temp,
@@ -79,7 +77,6 @@ class App extends React.Component {
   };
 
   countScore = () => {
-    //counting score for each player
     const tempCounter = [0, 0];
     //if indexWinner is 0, add +1 to count index 0, vice versa
     if (this.state.indexWinner === 0) {
@@ -87,7 +84,7 @@ class App extends React.Component {
     } else if (this.state.indexWinner === 1) {
       tempCounter[1]++;
     }
-    //update all the scores
+
     this.setState((prevState) => ({
       player1Score: prevState.player1Score + tempCounter[0],
       player2Score: prevState.player2Score + tempCounter[1],
@@ -95,21 +92,19 @@ class App extends React.Component {
       overallPlayer2Score: prevState.overallPlayer2Score + tempCounter[1],
     }));
 
-    //trigger a function to check who is the overall winner
     this.setState(() => {
       this.checkWinner();
     });
   };
 
   checkWinner = () => {
-    //to check who is the overall winner based on who has the highest score
     let tempWinner = -1;
     if (this.state.player1Score > this.state.player2Score) {
       tempWinner = 0;
     } else if (this.state.player2Score > this.state.player1Score) {
       tempWinner = 1;
-    } else if (this.state.player1Score === this.state.player2Score) {
-      tempWinner = 99;
+    } else {
+      tempWinner = -1;
     }
 
     this.setState({
@@ -118,77 +113,69 @@ class App extends React.Component {
   };
 
   render() {
-    //showing the card image
+    // const currCardElems = this.state.currCards.map(({ name, suit }) => (
+    //   <div key={`${name}${suit}`}>
+    //     {name} of {suit}
+    //   </div>
+    // ));
+
     const imageCard = this.state.currCards.map(({ name, suit }) => (
       <img
         src={require(`./PNG/${name.toLowerCase()}_of_${suit.toLowerCase()}.png`)}
         alt={`${name} of ${suit}`}
-        style={{ height: "200px" }}
+        style={{ height: "100px" }}
       />
     ));
 
     const numRoundsLeft = this.state.cardDeck.length / 2;
-    //overall winner in 26 rounds
     const gameWinnerMessage =
-      this.state.roundWinner === 99
+      this.state.roundWinner === -1
         ? "Tie"
         : `🏆Player ${this.state.roundWinner + 1} WON!🏆`;
 
-    //winner in each round
-    const winnerMessage =
-      this.state.indexWinner === -1
-        ? ""
-        : this.state.indexWinner === 99
-        ? `Tie`
-        : `Player ${this.state.indexWinner + 1} won!`;
-
     return (
-      <div className="container">
-        <div className="row mt-2">
-          <div className="col-10"></div>
-          <div className="col-2 text-right">
-            <p>{numRoundsLeft} rounds left</p>
-          </div>
-          <div className="text-center mt-1 mb-5">
-            <h3>High Card 🚀</h3>
-          </div>
+      <div className="App">
+        <header className="App-header">
+          <h3>High Card 🚀</h3>
 
-          <div className="col text-center">
-            {imageCard[0]}
-            <p>Player 1</p>
-            <h2>🏆{this.state.player1Score}</h2>
-            <p>Overall 🏆{this.state.overallPlayer1Score} </p>
-          </div>
+          {/* {currCardElems} */}
 
-          <div className="col text-center">
-            {imageCard[1]}
-            <p>Player 2</p>
-            <h2>🏆{this.state.player2Score}</h2>
-            <p>Overall 🏆{this.state.overallPlayer2Score}</p>
-          </div>
-        </div>
+          {imageCard[0]}
+          <p>Player 1</p>
+          {imageCard[1]}
+          <p>Player 2</p>
 
-        <div className="row">
-          <div className="col-12 text-center">
-            {numRoundsLeft > 0 ? (
-              <button className="btn btn-dark" onClick={this.dealCards}>
-                Deal
-              </button>
-            ) : (
-              <button className="btn btn-light" onClick={this.reset}>
-                Reset
-              </button>
-            )}
-          </div>
+          <br />
 
-          <h2 className="text-center mt-2">
-            {numRoundsLeft > 0 && winnerMessage}
-          </h2>
+          {numRoundsLeft > 0 ? (
+            <button onClick={this.dealCards}>Deal</button>
+          ) : (
+            <button onClick={this.reset}>Reset</button>
+          )}
 
-          <h1 className="text-center mt-2">
-            {numRoundsLeft === 0 && gameWinnerMessage}
-          </h1>
-        </div>
+          <p>
+            {this.state.indexWinner === -1
+              ? ""
+              : this.state.indexWinner === 99
+              ? `Tie`
+              : `Player ${this.state.indexWinner + 1} won!`}
+          </p>
+
+          <p>
+            Player 1 🏆{this.state.player1Score} -- Player 2 🏆
+            {this.state.player2Score}
+          </p>
+
+          <p>There are {numRoundsLeft} rounds left</p>
+
+          <h1>{numRoundsLeft === 0 && gameWinnerMessage}</h1>
+
+          <p>
+            Overall across games Player 1 🏆{this.state.overallPlayer1Score} --
+            Player 2 🏆
+            {this.state.overallPlayer2Score}
+          </p>
+        </header>
       </div>
     );
   }
